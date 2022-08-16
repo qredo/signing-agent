79d543e63308dffba79f3213c3cc2a2f1d0dca53
Andrei Sambra <andrei.sambra@qredo.com>
2022-08-16 08:01:29 +0000

    CE-31 added option to build images for multi arch

--- build.sh
+++ build.sh
@@ -9,17 +9,41 @@ VERSION="dev"
 rm -rf vendor
 
 dev_docker_build() {
-
-  docker build --build-arg BUILD_DATE="$BUILD_DATE" --build-arg BUILD_COMMIT="$GIT_COMMIT" --build-arg BUILD_VERSION="$VERSION" -t automated-approver:dev -f dockerfiles/Dockerfile .
+  docker build --build-arg BUILD_DATE="$BUILD_DATE" \
+                --build-arg BUILD_COMMIT="$GIT_COMMIT" \
+                --build-arg BUILD_VERSION="$VERSION" \
+                -t automated-approver:dev \
+                -f dockerfiles/Dockerfile .
+  
   rm -rf vendor
 }
 
 dev_docker_test_build() {
-
-  docker build --build-arg BUILD_DATE="$BUILD_DATE" --build-arg BUILD_COMMIT="$GIT_COMMIT" --build-arg BUILD_VERSION="$VERSION" -t automated-approver-unittest:dev -f dockerfiles/DockerfileUnitTest .
+  docker build --build-arg BUILD_DATE="$BUILD_DATE" \
+                --build-arg BUILD_COMMIT="$GIT_COMMIT" \
+                --build-arg BUILD_VERSION="$VERSION" \
+                -t automated-approver-unittest:dev \
+                -f dockerfiles/DockerfileUnitTest .
+  
   rm -rf vendor
 }
 
+dev_docker_build_multiarch() {
+  # We need to build the images one by one so they can be exported (doesn't work otherwise)
+  # If this command fails because of buildx, please run the following command:
+  # docker buildx create --use
+  for arch in amd64 arm64 ; do
+      docker buildx build \
+      --build-arg BUILD_DATE="$BUILD_DATE" \
+      --build-arg BUILD_COMMIT="$GIT_COMMIT" \
+      --build-arg BUILD_VERSION="$VERSION" \
+      --platform linux/$arch \
+      --output "type=docker,push=false,name=automated-approver:dev-$arch,dest=automated-approver-$arch.tar" \
+      -f dockerfiles/Dockerfile .
+  done
+
+  rm -rf vendor
+}
 
 
 dev_local_build() {
@@ -40,6 +64,9 @@ if [ -n "$1" ]; then
   case $1 in
     docker)
       dev_docker_build
+      ;;
+    docker_multiarch)
+      dev_docker_build_multiarch
       ;;
     docker_unittest)
       dev_docker_test_build
